22f7ed4c92f105a8efeba7dc733547ec174d15ae
pearsedarcy <pearse.darcy@outlook.com>
2023-12-20 17:09:20 +0100

    Add intial functions

---
+++ assets/js/script.js
@@ -0,0 +1,13 @@
+function runGame() {}
+
+function checkAnswer() {}
+function calculateCorrectAnswer() {}
+
+function incremementScore() {}
+
+function displayAdditionQuestion() {}
+
+function displaySubtractionQuestion() {}
+
+function displayMultiplyQuestion() {}
+function displayDivideQuestion() {}
